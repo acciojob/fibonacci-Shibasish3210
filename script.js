ac145e1db663dea177b3a50ad0b32174b56ec0be
Shibasish3210 <111530472+Shibasish3210@users.noreply.github.com>
2023-07-15 22:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function fibonacci(num) {
 // your code here
-}
+	if(num === 1 || num===0) return num;
 
-module.exports = fibonacci;
+	return fibonacci(num-2)+fibonacci(num-1);
+}
+console.log(fibonacci(8));
+
+// module.exports = fibonacci;
